d6c7e6789367c04da927d8839e11c4047317e01e
Arun  K <arunk.iq@gmail.com>
2023-08-31 23:06:19 +0530

    Update streamlit_app.py
    Trial to remove warning message

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.write("Input 3 numbers in below fields")
-number1 = st.number_input("First number", format='%d')
-number2 = st.number_input("Second number", format='%d')
-number3 = st.number_input("Third number", format='%d')
+number1 = st.number_input("First number", format='%d', step=1)
+number2 = st.number_input("Second number", format='%d', step=1)
+number3 = st.number_input("Third number", format='%d', step=1)
 if st.button('Get Max Number'):
     st.write("Maximum Number:",max(number1,number2,number3))
 
