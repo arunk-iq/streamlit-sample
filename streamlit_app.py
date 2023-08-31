c0e4e04557d2034dc8705cf70cd38850924a623f
Arun  K <arunk.iq@gmail.com>
2023-08-31 22:56:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ st.write("Input 3 numbers in below fields")
 number1 = st.number_input("First number")
 number2 = st.number_input("Second number")
 number3 = st.number_input("Third number")
+if st.button('Get Max Number'):
+    st.write(max(number1,number2,number3))
 
