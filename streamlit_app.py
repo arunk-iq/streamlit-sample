5c8b387d84532c15be1ab01b5cf177ca525bf5fd
Arun  K <arunk.iq@gmail.com>
2023-08-31 23:02:01 +0530

    Update streamlit_app.py
    First Bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ number1 = st.number_input("First number", format='%d')
 number2 = st.number_input("Second number", format='%d')
 number3 = st.number_input("Third number", format='%d')
 if st.button('Get Max Number'):
-    st.write("Maximum Number:',max(number1,number2,number3))
+    st.write("Maximum Number:",max(number1,number2,number3))
 
