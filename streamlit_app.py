f16a6c0723451d978373d87ba524d8333b7949a0
Arun  K <arunk.iq@gmail.com>
2023-08-31 22:53:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
+import streamlit as st
+st.write("Input 3 numbers in below fields")
+number1 = st.number_input("First number")
+number2 = st.number_input("Second number")
+number3 = st.number_input("Third number")
 
