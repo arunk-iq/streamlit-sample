22d6ae4b1f214639762422fac41a903f41227bac
Arun  K <arunk.iq@gmail.com>
2023-08-31 23:01:23 +0530

    Update streamlit_app.py
    Formatted Number

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.write("Input 3 numbers in below fields")
-number1 = st.number_input("First number")
-number2 = st.number_input("Second number")
-number3 = st.number_input("Third number")
+number1 = st.number_input("First number", format='%d')
+number2 = st.number_input("Second number", format='%d')
+number3 = st.number_input("Third number", format='%d')
 if st.button('Get Max Number'):
-    st.write(max(number1,number2,number3))
+    st.write("Maximum Number:',max(number1,number2,number3))
 
